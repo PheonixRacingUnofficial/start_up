ec9bd8284fbeb22208f0b2a942f89008835d6064
Colin Sadowitz <147172895+CJSadowitz@users.noreply.github.com>
2024-11-06 14:23:29 -0500

    Wait for display
    Updated Bash Script to wait for the display on the raspberry pi to turn on before the python code is called. This is to allow tkinter GUI to show up on screen

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import tkinter as tk
+import os
+
 def main():
-	print ("Hello World")
+	os.environ['DISPLAY'] = ':0'
+	try:
+		root = tk.Tk()
+		speed_text = tk.StringVar()
+		speed_text.set("Hello World")
+
+		speed_label = tk.Label(root, textvariable=speed_text, font=("Arial",14))
+
+		speed_label.pack()
+		print ("Entered Main Loop")
+		root.mainloop()
+	except Exception as e:
+		print ("Tkinter Failed", e)
+
+	print ("End of file")
 
 if __name__ == "__main__":
 	main()
--- startup.sh
+++ startup.sh
@@ -1,8 +1,37 @@
-echo "Initializing Python"
-cd {PATH}
+date >> bash_log.txt
+echo "Naviagating to Directory" >> bash_log.txt
+cd /home/solar/Documents/startup
+echo "Initializing Python" >> bash_log.txt
 source venv/bin/activate
-echo "Activated venv"
+echo "Activated venv" >> bash_log.txt
 pip install -r requirements.txt
-echo "Installed dependencies"
-python3 main.py
-echo "GUI Started"
+echo "Installed dependencies" >> bash_log.txt
+check_display()
+{
+	if [[ -n "$DISPLAY" ]]; then
+		echo "Display on" >> bash_log.txt
+		return 0
+	else
+		echo "Display off" >> bash_log.txt
+		return 1
+	fi
+}
+
+is_display_on()
+{
+	for i in $(seq 1 10);
+	do
+		echo "Checking Display" >> bash_log.txt
+		check_display
+		if [ $? -eq 0 ]; then
+			echo "Successfully found Display" >> bash_log.txt
+			break
+		fi
+		echo "Slept: " "$1" >> bash_log.txt
+		sleep 5
+	done
+}
+is_display_on
+export DISPLAY=:0
+python3 main.py > log.txt
+echo "GUI Started" >> bash_log.txt
